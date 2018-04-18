cbeb199da0d266338501e75d642e8a7270c8e902
Ricardo Domingos <ricardohsd@gmail.com>
2018-04-18 22:48:46 +0200

    Add authentication middleware with goji/httpauth

--- routes.go
+++ routes.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	"github.com/goji/httpauth"
 	"github.com/gorilla/mux"
 )
 
@@ -15,12 +16,15 @@ type Route struct {
 
 type Routes []Route
 
+var authHandler = httpauth.SimpleBasicAuth("username", "password")
+
 func NewRouter() *mux.Router {
 	router := mux.NewRouter().StrictSlash(true)
 	for _, route := range routes {
 		var handler http.Handler
 
 		handler = route.HandlerFunc
+		handler = authHandler(handler)
 		handler = Logger(handler, route.Name)
 		handler = JsonHeader(handler)
 
